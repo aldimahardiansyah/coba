c0b0bb03b02825e51a9cc49e839953273b90c8bc
aldimahardiansyah <aldimdiansyah@gmail.com>
2023-12-11 16:47:36 +0700

    belajar js

---
+++ praktik11/parkir.js
@@ -0,0 +1,23 @@
+function biayaParkir() {
+    // tangkap inputtan user
+    let masuk = document.querySelector("#masuk").value
+    let keluar = document.querySelector("#keluar").value
+
+    // hitung durasi parkir
+    let durasi = keluar - masuk
+
+    let biaya = 3000
+
+    // looping durasi untuk menghitung biaya
+    for (let i = 2; i < durasi; i++) {
+        biaya += 1000
+    }
+
+    // tangkap elemen untuk menampilkan hasil
+    let hasilDurasi = document.querySelector("#hasilDurasi")
+    let totalBiaya = document.querySelector("#totalBiaya")
+
+    // tampilkan hasil
+    hasilDurasi.innerHTML = durasi
+    totalBiaya.innerHTML = biaya
+}
